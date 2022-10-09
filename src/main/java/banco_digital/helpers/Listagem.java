5dd5383ba05e58621fcf17cdfa3f7ed5bb1224f2
Ricardo <ricardomr8@hotmail.com>
2022-10-09 20:07:09 -0300

    new methods to print Accounts added.

--- src/main/java/banco_digital/helpers/Listagem.java
+++ src/main/java/banco_digital/helpers/Listagem.java
@@ -1,20 +1,21 @@
 package banco_digital.helpers;
 
 import banco_digital.domain.*;
+import org.jetbrains.annotations.NotNull;
 
 public class Listagem {
     /**
      * Lista todos os clientes do banco
      * @param b o banco de onde serão retiradas as informações de clientes.
      */
-    public void listaClientes(Banco b) {
+    public void listaClientes(@NotNull Banco b) {
         System.out.println("============ Listagem de clientes ============");
         for (Cliente cliente :  b.getClientes()) {
             System.out.println(cliente);
         }
     }
 
-    public void listaInfoConta(Banco b) {
+    public void listaInfoConta(@NotNull Banco b) {
         System.out.println("======================== Informações de Conta ========================");
         for (Conta conta : b.getContas()) {
             System.out.println(conta);
@@ -25,7 +26,7 @@ public class Listagem {
      * Utiliza o método de impressão de conta Corrente para as contas Correntes no Banco
      */
 
-    public void imprimirExtratoContaCorrente(Banco b) {
+    public void imprimirExtratoContaCorrente(@NotNull Banco b) {
         System.out.println("====================== Extrato de Conta Corrente ======================");
         for (Conta conta: b.getContas()) {
             if (conta instanceof ContaCorrente)
@@ -36,7 +37,7 @@ public class Listagem {
     /**
      * Utiliza o método de impressão de conta Poupança para as contas Poupancas no banco
      */
-    public void imprimirExtratoContaPoupanca(Banco b) {
+    public void imprimirExtratoContaPoupanca(@NotNull Banco b) {
         System.out.println("====================== Extrato de Conta Poupanca ======================");
         for (Conta conta: b.getContas()) {
             if (conta instanceof ContaPoupanca) {
